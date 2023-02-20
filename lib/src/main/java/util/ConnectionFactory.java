7bd5d34fe1eace4e6f7746d097ed5c322ed9af7b
Matheus Victor <matheusvictor.dev@gmail.com>
2023-02-20 11:00:00 -0300

    Classe que define e realiza a conexão com o banco de dados

--- lib/src/main/java/util/ConnectionFactory.java
+++ lib/src/main/java/util/ConnectionFactory.java
@@ -3,6 +3,7 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * 
@@ -12,9 +13,13 @@ import java.sql.PreparedStatement;
 
 public class ConnectionFactory {
 	
+	/*Driver de conexão do db com o Java */
 	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	public static final String URL = "jdbc:mysql://localhost:3306/dbtaskhub";
+	/*Onde meu banco de dados está rodando: localização, porta e o nome do db*/
+	public static final String URL = "jdbc:mysql://localhost:3306/taskhub";
+	/* Usuário do db*/
 	public static final String USER = "root";
+	/* Senha de acesso ao db*/
 	public static final String PASS = "";
 	
 	public static java.sql.Connection getConnection() {
@@ -43,6 +48,19 @@ public class ConnectionFactory {
 			
 			if(statement != null) {
 				statement.close();
+			}
+				
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", e);
+		}		
+	}
+	
+	public static void closeConnection(Connection connection, PreparedStatement statement, ResultSet resultSet) {		
+		closeConnection(connection, statement);		
+		try {
+			
+			if(resultSet != null) {
+				resultSet.close();
 			}
 				
 		} catch (Exception e) {
